9616ccc154a8cbe9c073e3250aa1147184df951c
Pragathi.N <pragathipragathi551@gmail.com>
2025-11-24 07:44:31 +0530

    Add files via upload

---
+++ linear search ex.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int arr[5]={12,23,35,47,59};
+int n=5;
+int key,i,found=0;
+
+printf("Enter element to search:");
+scanf("%d",key);
+
+for(i=0;i<n;i++){
+    if(arr[i]==key){
+    printf("Element %d found at position %d\n",key,i+1);
+    found=i;
+    break;
+    }
+}
+if(found)
+    printf("Element not found\n");
+
+    return 0;
+}
